f6426de8acd2bfaefc82251ae870f1cab375c14b
Tu Tran <tu.tran@temple.edu>
2017-11-29 02:16:56 -0500

    add test command in manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 
 from flask_script import Manager
 from app import create_app
@@ -9,6 +10,22 @@ manager = Manager(app)
 
 # check if the environment configuration is production
 is_prod = (os.environ.get('APP_SETTINGS') == 'production')
+
+
+@manager.command
+def test(type=""):
+    """Run the unit tests without code coverage."""
+    try:
+        tests = unittest.TestLoader().discover(
+            f'tests/{type}',
+            pattern='test*.py'
+        )
+        result = unittest.TextTestRunner(verbosity=2).run(tests)
+        if result.wasSuccessful():
+            return 0
+        return 1
+    except ImportError:
+        print("There is no test like that!")
 
 
 if __name__ == "__main__":
