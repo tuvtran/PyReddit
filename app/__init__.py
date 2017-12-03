617557bb67bbe3a891bf47229409121930000524
Tu Tran <tu.tran@temple.edu>
2017-12-03 02:34:19 -0500

    add user models

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 # local import
 from instance.config import app_config
 
 # initialize the db instance
 db = SQLAlchemy()
+
+# initialize bcrypt
+bcrypt = Bcrypt()
 
 
 def create_app(config_name):
@@ -14,6 +18,7 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
 
     db.init_app(app)
+    bcrypt.init_app(app)
 
     # import blueprints here
     from app.views import views_bp
---
+++ app/models/user.py
@@ -0,0 +1,26 @@
+import os
+from app import db, bcrypt
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(
+        db.Datetime, nullable=False, default=db.func.current_timestamp()
+    )
+
+    def __repr__(self):
+        return f'<User: {self.name}>'
+
+    def save(self):
+        # generate a hash in the database
+        self.password = bcrypt.generate_password_hash(
+            self.password, os.environ.get('BCRYPT_LOG_ROUNDS', 4)
+        ).decode()
+
+        db.session.add(self)
+        db.session.commit()
