d9309bd22690c951558eda93ca47e2eaaf2a0f83
Tu Tran <tu.tran@temple.edu>
2017-12-10 07:44:49 -0500

    add test for karma updating after being upvoted for post and comment

--- tests/unit/test_user.py
+++ tests/unit/test_user.py
@@ -1,7 +1,7 @@
 from app import bcrypt
 from app.models.user import User
-from app.models.thread import Link
-from app.models.comment import Comment
+from app.models.thread import Link, ThreadUpvote
+from app.models.comment import Comment, CommentUpvote
 from tests.unit.base import BaseUnitTest
 
 
@@ -41,5 +41,29 @@ class UserModelTest(BaseUnitTest):
 
         self.assertEqual(User.query.get(1).comment_karma, 10)
 
-    def test_user_has_karma_after_being_upvoted(self):
-        pass
+    def test_user_has_karma_after_being_upvoted_post(self):
+        l = Link(
+            title='Test link',
+            link='http://google.com',
+            user_id=1,
+            subreddit_id=1
+        )
+        l.save()
+
+        for i in range(10):
+            ThreadUpvote(link_id=l.id, user_id=i*10).save()
+
+        self.assertEqual(User.query.get(1).post_karma, 11)
+
+    def test_user_has_karma_after_being_upvoted_comment(self):
+        c = Comment(
+            content="This is a test comment",
+            user_id=1,
+            link_id=1
+        )
+        c.save()
+
+        for i in range(10):
+            CommentUpvote(comment_id=c.id, user_id=i*10).save()
+
+        self.assertEqual(User.query.get(1).comment_karma, 11)
