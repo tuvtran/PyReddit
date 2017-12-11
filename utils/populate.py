260252df7fe13f81f8d9eb1da56e85aaf5901c28
Tu Tran <tu.tran@temple.edu>
2017-12-11 05:57:44 -0500

    comment view

--- utils/populate.py
+++ utils/populate.py
@@ -1,6 +1,7 @@
 from app.models.user import User
 from app.models.subreddit import Subreddit
 from app.models.thread import Link, Text
+from app.models.comment import Comment
 
 
 def generate_data():
@@ -18,7 +19,11 @@ def generate_data():
         Subreddit(name=sub[0], description=sub[1]).save()
 
     # create a test user
-    User(name='admin', email='admin@pyreddit.com', password='testing').save()
+    User(name='admin', email='admin@pyreddit.com', password='test').save()
+    User(name='mod1', email='mod1@pyreddit.com', password='test').save()
+    User(name='datguy', email='a@pyreddit.com', password='test').save()
+    User(name='thechosenone', email='b@pyreddit.com', password='test').save()
+    User(name='obama', email='c@pyreddit.com', password='test').save()
 
     # Create some links
     texts = [
@@ -41,3 +46,43 @@ def generate_data():
 
     for l in links:
         Link(title=l[0], link=l[1], user_id=l[2], subreddit_id=l[3]).save()
+
+    add_comment_link(1)
+
+
+def add_comment_link(id):
+
+    c1 = Comment(
+        content="This link is awesome",
+        user_id=1,
+        link_id=id
+    )
+    c1.save()
+
+    c2 = Comment(
+        content='This',
+        user_id=2,
+        comment_id=c1.id
+    )
+    c2.save()
+
+    c3 = Comment(
+        content="Upvote instead of comment 'this' lmao",
+        user_id=3,
+        comment_id=c2.id
+    )
+    c3.save()
+
+    c4 = Comment(
+        content="I read the same thing last night too",
+        user_id=4,
+        link_id=id
+    )
+    c4.save()
+
+    c5 = Comment(
+        content="Really?????",
+        user_id=1,
+        comment_id=c4.id
+    )
+    c5.save()
